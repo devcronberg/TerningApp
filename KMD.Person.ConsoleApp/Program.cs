76df938a699578c975ab5d7a16dad4b2cec74f0f
Michell Cronberg <michell@cronberg.dk>
2020-09-02 09:48:21 +0200

    excel

---
+++ KMD.Person.ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using KMD.Person.Kerne;
+using System;
+using System.Collections.Generic;
+
+namespace KMD.Person.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //List<Person> lst = ...
+
+            //Kerne.Person p = new Kerne.Person();
+            //p.Id = 1;
+            //p.Navn = "a";
+            //p.Land = "DK";
+            //p.Biler.Add(new Kerne.Bil { Id = 1, Mærke = "x", Person = p });
+
+            //Console.WriteLine(p.Navn);
+            //foreach (var bil in p.Biler)
+            //    Console.WriteLine(bil.Mærke);
+
+            //Console.WriteLine(p.Biler[0].Person.Navn);
+
+            ExcelRepository r = new ExcelRepository();
+            r.HentPersoner();
+        }
+    }
+}
---
+++ KMD.Person.Kerne/Person.cs
@@ -0,0 +1,56 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+
+namespace KMD.Person.Kerne
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Navn { get; set; }
+        public string Land { get; set; }
+        public List<Bil> Biler { get; set; }
+
+        public Person()
+        {
+            Biler = new List<Bil>();
+        }
+
+    }
+
+    public class Bil
+    {
+        public int Id { get; set; }
+        public string Mærke { get; set; }
+        public Person Person { get; set; }
+
+    }
+
+    public interface IDataRepository
+    {
+        List<Person> HentPersoner();
+    }
+
+    public class ExcelRepository : IDataRepository
+    {
+        public List<Person> HentPersoner()
+        {
+            DataSet ds;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (var stream = File.Open(@"c:\tmp\personer.xlsx", FileMode.Open, FileAccess.Read))
+            using (var reader = ExcelReaderFactory.CreateReader(stream))
+                ds = reader.AsDataSet(new ExcelDataSetConfiguration()
+                {
+                    ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                    {
+                        UseHeaderRow = true
+                    }
+                });
+            return null;
+        }
+    }
+
+
+}
