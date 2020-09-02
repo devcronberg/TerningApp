4edbbd20929850b315445c73035be94ce8614355
Michell Cronberg <michell@cronberg.dk>
2020-09-02 15:12:01 +0200

    .

---
+++ WinFormDemo2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormDemo2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var lst = DRNyheder.Kerne.DRNyhed.Hent("https://www.dr.dk/nyheder/service/feeds/allenyheder/");
+            DateTime a = monthCalendar1.SelectionRange.Start;
+            textBox1.Text = a.ToLongDateString();
+            textBox1.BackColor = System.Drawing.Color.Red;
+            dataGridView1.DataSource = lst;
+            
+        }
+    }
+}
